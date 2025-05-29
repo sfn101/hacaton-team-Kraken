86aa2d2fd88381c3e6d794369334338fb0a33d23
soufiane <amabraa.soufiane@gmail.com>
2025-05-29 02:59:53 +0100

    removing use stat

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-//components
-import SwipeSlides from './components/SwipeSlides'
+// Components
+import SwipeSlides from './components/SwipeSlides';
 
 function App() {
+
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Constrained main content */}
-      <div id="root" className="">
-        <main>
-        
+    <div className="flex flex-col min-h-screen w-full">
+      <main className="flex-grow w-full">
         <SwipeSlides />
-        </main>
-      </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
